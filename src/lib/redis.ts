bf8c553b11ed239692ad9fd94f16a925fc4691ba
kishon45229 <kishon45229@outlook.com>
2026-01-18 14:30:34 +0530

    Removed redis.ts

--- src/lib/redis.ts
+++
@@ -1,7 +0,0 @@
-import { Redis } from "@upstash/redis";
-import env from "@/lib/env";
-
-export const upstashRedis = new Redis({
-  url: env.UPSTASH_REDIS_REST_URL,
-  token: env.UPSTASH_REDIS_REST_TOKEN,
-});
