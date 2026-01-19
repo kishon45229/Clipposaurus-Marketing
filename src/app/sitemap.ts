e66aa80f1693c32c5943d9652cf2f54d9637fe44
kishon45229 <kishon45229@outlook.com>
2026-01-19 14:40:40 +0530

    Remove contact in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -32,12 +32,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
-    },
-    {
-      url: `${MARKETING_URL}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
+    }
   ];
 }
