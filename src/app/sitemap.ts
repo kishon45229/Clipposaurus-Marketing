ed9173d85c5492adb72a99d8ae510512458f0cd3
kishon45229 <kishon45229@outlook.com>
2026-01-19 10:55:24 +0530

    Update sitemap by import MARKETING_URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,44 +1,40 @@
 import { MetadataRoute } from "next";
-
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_BASE_URL
-    : process.env.NEXT_PUBLIC_DEVELOPMENT_BASE_URL;
+import { MARKETING_URL } from "@/lib/urls";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: BASE_URL!,
+      url: MARKETING_URL!,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: `${BASE_URL}/about`,
+      url: `${MARKETING_URL}/about`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: `${BASE_URL}/faq`,
+      url: `${MARKETING_URL}/faq`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: `${BASE_URL}/terms-of-service`,
+      url: `${MARKETING_URL}/terms-of-service`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: `${BASE_URL}/privacy-policy`,
+      url: `${MARKETING_URL}/privacy-policy`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: `${BASE_URL}/contact`,
+      url: `${MARKETING_URL}/contact`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
